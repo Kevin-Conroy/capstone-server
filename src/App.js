9e3c418852d4eb309c5d62f7e65864639e401e06
Kevin Conroy <Kcdrumming@gmail.com>
2021-05-16 14:53:41 -0700

    Initial commit - basic CRA template set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className='App'>
+      <div>
+        <h1>Capstone Server</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
